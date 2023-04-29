1a92e8e0a5c0906f373735ed786730c559a72d87
BillRizer <teliz.pablo@gmail.com>
2023-04-29 20:18:33 -0300

    fix: input tests after refactor to use theme.props

--- src/presentation/shared/components/Input/index.spec.tsx
+++ src/presentation/shared/components/Input/index.spec.tsx
@@ -1,35 +1,38 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { InputComponent } from ".";
+import { MockTheme } from "../../../../global/utils/test-wrap-theme";
 
 describe("Input component", () => {
   it("renders a label and input element", () => {
     render(
-      <InputComponent
-        label="Email"
-        id="email"
-        type="email"
-        value=""
-        onChange={() => {}}
-      />
+      <MockTheme>
+        <InputComponent
+          label="Email"
+          id="email"
+          type="email"
+          value=""
+          onChange={() => {}}
+        />
+      </MockTheme>
     );
-    const labelElement = screen.getByLabelText("Email");
-    const inputElement = screen.getByLabelText("Email");
-    expect(labelElement).toBeInTheDocument();
+    const inputElement = screen.getByRole("textbox");
     expect(inputElement).toBeInTheDocument();
   });
 
   it("updates the value when the input changes", () => {
     const handleChange = jest.fn();
     render(
-      <InputComponent
-        label="Email"
-        id="email"
-        type="email"
-        value=""
-        onChange={handleChange}
-      />
+      <MockTheme>
+        <InputComponent
+          label="Email"
+          id="email"
+          type="email"
+          value=""
+          onChange={handleChange}
+        />
+      </MockTheme>
     );
-    const inputElement = screen.getByLabelText("Email");
+    const inputElement = screen.getByRole("textbox");
     fireEvent.change(inputElement, { target: { value: "test@example.com" } });
     expect(handleChange).toHaveBeenCalledTimes(1);
   });
