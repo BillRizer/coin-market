b59bca598016a11dd8b0a8228661fe41d620ed7c
BillRizer <teliz.pablo@gmail.com>
2023-04-29 13:45:19 -0300

    feat: add global reference for base theme

---
+++ src/global/styles/theme-base.ts
@@ -0,0 +1,31 @@
+export const themeBase = {
+  name: "theme-base",
+  colors: {
+    primary: {
+      base: "#FBAB34",
+      w400: "#FFB94F",
+    },
+    secondary: {
+      base: "#8C8A97",
+    },
+    tertiary: {
+      base: "#1BD171",
+    },
+    quartenary: {
+      base: "#EC3237",
+    },
+    white: "#fff",
+    black: "#000",
+    textBase: "#5D6670",
+  },
+  fonts: {
+    h1: { size: "48px", LineHeight: "56", LetterSpacing: "-1" },
+    h2: { size: "40px", LineHeight: "48", LetterSpacing: "-1" },
+    h3: { size: "32px", LineHeight: "40", LetterSpacing: "0" },
+    h4: { size: "24px", LineHeight: "32", LetterSpacing: "0" },
+    h5: { size: "20px", LineHeight: "24", LetterSpacing: "0" },
+    p: { size: "16px", LineHeight: "24", LetterSpacing: "0" },
+    label: { size: "14px", LineHeight: "16", LetterSpacing: "0" },
+    smallLabel: { size: "12px", LineHeight: "14", LetterSpacing: "0" },
+  },
+};
