30ea0d5e0796a7544a59038e0d078d194193223d
BillRizer <teliz.pablo@gmail.com>
2023-05-01 10:18:57 -0300

    fix: configure devices and other little things

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React from "react";
 
 import { RoutesComponent } from "./application/router";
 import AppProvider from "./application/hook/index";
+import GlobalStyle from "./global/styles/global-style";
+import { setConfiguration } from "react-grid-system";
+setConfiguration({
+  containerWidths: [272, 672, 1216],
+  breakpoints: [302, 702, 1246],
+  gutterWidth: 32,
+  gridColumns: 12,
+  defaultScreenClass: "lg",
+  maxScreenClass: "lg",
+});
 
 function App() {
   return (
     <>
       <AppProvider>
+        <GlobalStyle />
         <RoutesComponent></RoutesComponent>
       </AppProvider>
     </>
--- src/application/router/index.tsx
+++ src/application/router/index.tsx
@@ -2,16 +2,16 @@ import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import { NotAllowedPage } from "../../presentation/pages/NotAllowedPage";
 import { NotFoundPage } from "../../presentation/pages/NotFoundPage";
-import { LayoutContainer } from "../../presentation/shared/containers/Layout";
+import { PublicLayoutContainer } from "../../presentation/shared/layount/PublicLayout";
+import { LoggedLayoutContainer } from "../../presentation/shared/layount/LoggedLayout";
 import { HomePage } from "../../presentation/pages/Home";
+import { LandPage } from "../../presentation/pages/LandPage";
 
 export const RoutesComponent = () => {
   return (
     <Routes>
-      <Route path="/" element={<LayoutContainer></LayoutContainer>}>
-        <Route index element={<HomePage />} />
-        <Route path="/about" element={<>about</>} />
-        <Route path="/dashboard" element={<>dashboard</>} />
+      <Route path="/" element={<PublicLayoutContainer></PublicLayoutContainer>}>
+        <Route index element={<LandPage />} />
 
         <Route
           path="/not-allowed"
@@ -19,7 +19,7 @@ export const RoutesComponent = () => {
         />
         <Route path="/*" element={<NotFoundPage></NotFoundPage>} />
       </Route>
-      <Route path="/admin" >
+      <Route path="/admin" element={<LoggedLayoutContainer></LoggedLayoutContainer>}>
         <Route index element={<HomePage />} />
       </Route>
     </Routes>
